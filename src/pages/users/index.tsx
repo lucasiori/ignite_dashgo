ab294f6e890cce64ad9ae08193371a6867d0b285
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-02 14:16:25 -0300

    feat: configure axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,14 +18,14 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { api } from '../../services/api';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+    const { data } = await api.get('/users');
     
     const users = data.users.map(user => ({
       id: user.id,
